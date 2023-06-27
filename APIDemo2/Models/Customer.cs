348fd6b5d39508dc59b40a0e3a761d6f8922941a
Alumno <Alumno@LAB707-04>
2023-06-26 21:17:24 -0500

    Agregar archivos de proyecto.

---
+++ APIDemo2/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APIDemo2.Models
+{
+    public class Customer
+    {
+
+        public int CustomerID { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+        public List<Invoice>? Invoices { get; set; }
+    }
+
+}
